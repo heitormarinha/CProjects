bd18d3e83335fa01eeafd61f34721724ec7fa674
heitor <heitor.mpr@gmail.com>
2022-01-30 20:10:05 -0500

    commit

--- learnC/binarySearch/binarySearch.c
+++ learnC/binarySearch/binarySearch.c
@@ -4,15 +4,22 @@ int binsearch(int x, int v[], int n)
     int low, high, mid;
     low = 0;
     high = n - 1;
-    while (low<= high) {
-    mid= (low+high) / 2;
-    if (x < v[mid])
-
+    while (low<= high)
+    {
+        mid= (low+high) / 2;
+        if (x < v[mid])
+        {
+            high = mid - 1 ;
+            
+        }
+        else if (x > v[mid])
+        {
+            low = mid + 1;
+        }
+        else /* found match*/
+        {
+            return mid;
+        }
     }
-    high = mid - 1 ;
-    else if (x > v[mid])
-    low = mid + 1;
-    else /* found match*/
-    return mid;
     return -1; /* no match*/
 }
